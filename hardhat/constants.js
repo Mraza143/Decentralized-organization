141a7ac313ba26c83a3afd87656f7642ab458668
Raza <mraza6601@gmail.com>
2022-05-01 02:54:35 +0500

    first commit

---
+++ hardhat/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x007C5eE4Ce64b5baF90C79830e1262577EdffB07";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
